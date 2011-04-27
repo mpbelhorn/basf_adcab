635708675653bbd1cb2bf51b8fd8a305d3165a30
Matt Belhorn <matt.belhorn@gmail.com>
2011-04-27 15:00:05 -0400

    Modified the test submission script to analyze R. Louvot's signal MC.

--- run/sub.diagnostic.test.sh
+++ run/sub.diagnostic.test.sh
@@ -6,45 +6,23 @@
 #   output filename variables for description of data.
 
 AMOD=Adcab
-ADIR=/bwf/g64home/mbelhorn/analysis/adcab
-
-# The following variables set the output filenames of the run.
-# DATATYPE is a four letter identifier for the three data types:
-#   (G)e(N)eric(MC) (S)i(G)nal(MC) (DATA).
-# REGIME is a single number N to give the Y(NS) energy regime.
-#   N = 1 2 3 4 5
-# RUNTYPE is a two letter identifier for the processed MDST files.
-#   (SC)an (O)n(R)esonance (CO)ntinuum
-# EXPNO is the experiment number.
-#   Inlude leading 0 if necessary.
-# RUNNO is the starting run number.
-#   0 for MC
+ANALYSISDIR=/bwf/g64home/mbelhorn/analysis/adcab
+OUTPUTDIR=${ANALYSISDIR}/output
 DATATYPE=SGMC
-REGIME=5
-RUNTYPE=OR
-EXPNO=53
-RUNNO=000000
-#     ###### <- Match six digits in RUNNO for consistancy.
-
-# The following variables are only used for SIGMC 
-SIGMCPATH=/bwf/g64home/mbelhorn/analysis/MC/signalmc-u5s/mdst
-SIGMCMDST=adcab.vss-e53-n50600
-SIGMCMODEL=VSS
-
-# The following variables should be fixed.
+SIGMCPATH=/bwf/g68home/louvot/belle/fs/mcprod/mdstfile
+SIGMCMDST='evtgen-u5stobb-0-evt_RL-DECAY_BDlnu.dec-20101108_1839_new_e000067.081110_1846'
 OUTPUTNAME=${AMOD}.${DATATYPE}-diagnostic
-HBKFILE=${OUTPUTNAME}.hbk
 
-basf << EOF >& ${ADIR}/output/${OUTPUTNAME}.log
+basf << EOF >& test.log
 path create main
 path add_module main fix_mdst
 path create analysis
-path add_module analysis ${AMOD}
+path add_module analysis Adcab
 path add_condition main <=:0:KILL
 path add_condition main >:0:analysis
 initialize
-histogram define ${ADIR}/output/${HBKFILE}
-process_event ${SIGMCPATH}/${SIGMCMDST}-f0.mdst
+histogram define ${OUTPUTDIR}/hbks/${OUTPUTNAME}.hbk
+process_event ${SIGMCPATH}/${SIGMCMDST}.mdst
 output close
 terminate
 EOF
