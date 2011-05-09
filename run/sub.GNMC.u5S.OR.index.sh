87b2b41d41d2aef394a01c05b26832cfacc8b1b3
Matt Belhorn <matt.belhorn@gmail.com>
2011-05-09 01:23:08 -0400

    More work on index submission scripts.

--- run/sub.GNMC.u5S.OR.index.sh
+++ run/sub.GNMC.u5S.OR.index.sh
@@ -3,10 +3,10 @@
 # Author: M.P. Belhorn
 # Date: 2011.05.09
 
-if [ $# -ne 4 ]
+if [ $# -ne 3 ]
   then
     echo "Wrong number of arguments."
-    echo " Usage: $0 exp type stream filename"
+    echo " Usage: $0 exp type stream"
     echo " exp: 43 53 67 69 71"
     echo " type: bsbs charm nonbsbs uds"
     echo " stream: 0 1 2 3 4 5"
@@ -15,39 +15,10 @@ if [ $# -ne 4 ]
     EXPNO=$1
     EVENTTYPE=$2
     STREAMNO=$3
-    MDSTFILE=$4
     MDSTPATH=/bwf/g89home/g0mc/skim/skim5S/index/dilepskim/5S_onresonance/e0000${EXPNO}/evtgen-${EVENTTYPE}/s0${STREAMNO}
 fi
 
-if [ ! -f ${MDSTPATH}/${MDSTFILE}.index ]
-  then
-    echo " File does not exist. Aborting job sumbmission."
-    exit 1
-  else
-    ANALYSISDIR=/bwf/g64home/mbelhorn/analysis/adcab
-    OUTPUTDIR=${ANALYSISDIR}/output
-    RUNSTART=`echo ${MDSTFILE} | cut -c 19-24`
-    RUNEND=`echo ${MDSTFILE} | cut -c 26-31`
-    OUTPUTNAME=Adcab.GNMC.OR.s0${STREAMNO}.${EVENTTYPE}.e${EXPNO}.rs${RUNSTART}.re${RUNEND}
-    echo "Submitting job for ${OUTPUTNAME}"
-fi
-
-basf<< EOF >& ${OUTPUTDIR}/logs/${OUTPUTNAME}.log
-path create main
-module register fix_mdst
-path add_module main fix_mdst
-
-path create analysis
-module register Adcab
-path add_module analysis Adcab
-module put_parameter Adcab JPsi_Veto_OS_Only\0
-module put_parameter Adcab Verbose_Log\0
-
-path add_condition main <=:0:KILL
-path add_condition main >:0:analysis
-
-initialize
-histogram define ${OUTPUTDIR}/hbks/${OUTPUTNAME}.hbk
-process_event ${MDSTPATH}/${MDSTFILE}.index
-terminate
-EOF
+for MDSTFILE in $MDSTPATH/*.index
+do
+  echo "Processing $MDSTFILE..."
+done
