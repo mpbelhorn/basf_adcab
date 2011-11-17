3119ddab74acb676374142212ced44a74343c150
Matt Belhorn <matt.belhorn@gmail.com>
2011-11-17 15:00:34 -0500

    Created scripts to process MC.

---
+++ run/process_all_cont_gnmc.py
@@ -0,0 +1,21 @@
+#!/bwf/g64home/mbelhorn/.local/bin/python3
+import subprocess
+
+experiments = [43, 53, 67, 69, 71]
+streams = range(3)
+queue = 'index'
+
+for experiment in experiments:
+  for stream in streams:
+    lsf_args = [
+        'bsub', '-q', queue,
+        './run_basf.py',
+        '-gc',
+        '-s', str(stream),
+        '-e', str(experiment)]
+    log_args = lsf_args[3:]
+    log_args.append('-t')
+    log_process = subprocess.Popen(log_args)
+    log_process.wait()
+    lsf_process = subprocess.Popen(lsf_args)
+    lsf_process.wait()
