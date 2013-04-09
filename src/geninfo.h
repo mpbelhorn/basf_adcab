dea8af7c8ea4a912bcfbcaaa37690d313d8f5a14
Matt Belhorn <matt.belhorn@gmail.com>
2013-04-09 13:59:12 -0400

    Cleaned up generator information functions header

--- src/geninfo.h
+++ src/geninfo.h
@@ -13,16 +13,20 @@ int appendGenFSP(const Gen_hepevt &gen, std::vector<Gen_hepevt> &children);
 int dumpGenFSP(const Gen_hepevt &gen);
 int commonMother(std::vector<int> &mothers);
 int findCommonMother(Gen_hepevt pThis, Gen_hepevt pOther, int level);
-int compareFinalStates(std::vector<Gen_hepevt> reconstructed, std::vector<Gen_hepevt> generated);
-void fillMothers(Particle &p, std::vector<int> &thisMothers, std::vector<int> &otherMothers);
-void fillMothers(Particle &A, Particle &B, std::vector<int> &thisMothers, std::vector<int> &otherMothers);
-void fillMothers(Particle &A, Particle &B, Particle &C, std::vector<int> &thisMothers, std::vector<int> &otherMothers);
+int compareFinalStates(std::vector<Gen_hepevt> reconstructed,
+    std::vector<Gen_hepevt> generated);
+void fillMothers(Particle &p, std::vector<int> &thisMothers,
+    std::vector<int> &otherMothers);
+void fillMothers(Particle &A, Particle &B, std::vector<int> &thisMothers,
+    std::vector<int> &otherMothers);
+void fillMothers(Particle &A, Particle &B, Particle &C,
+    std::vector<int> &thisMothers, std::vector<int> &otherMothers);
 
 int getCommonMother(Particle &A, Particle &B);
 int getCommonMother(Particle &A, Particle &B, Particle &C);
 
-int findCommonMother(int nChildren, std::vector<int> thisMothers, std::vector<int> otherMothers);
-
+int findCommonMother(int nChildren, std::vector<int> thisMothers,
+    std::vector<int> otherMothers);
 
 void genDecayChain(Particle p, int* dChain);
 
